3fc0390b9e46453d220a185a4c1b8bc314e708cf
douglasschantz <schantzdouglas@gmail.com>
2023-08-20 14:11:02 -0300

    Create project

---
+++ src/main/java/com/github/schantz/ems/entity/Employe.java
@@ -0,0 +1,31 @@
+package com.github.schantz.ems.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="employes")
+public class Employe {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="email_id", nullable = false, unique=true)
+	private String email;
+}
---
+++ src/test/java/com/github/schantz/ems/EmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.schantz.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
